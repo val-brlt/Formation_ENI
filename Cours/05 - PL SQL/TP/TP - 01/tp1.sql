8e87470a37c557ac72ab869f50954182b9a0e5b4
Valentin Brault <valentin.brault0@gmail.com>
2021-06-30 11:15:10 +0200

    d

--- Cours/05 - PL SQL/TP/TP - 01/tp1.sql
+++ Cours/05 - PL SQL/TP/TP - 01/tp1.sql
@@ -1,56 +1,93 @@
-CREATE DATABASE BDD_TP
-GO
 
--- 1.2 Mise en place des tables
+-- 1.1 et 1.2
 
-create table genres(
-code char(5) constraint pk_genres primary key,
-libelle varchar2(80) not null);
-
-create table ouvrages(
-isbn number(10) constraint pk_ouvrages primary key,
-titre varchar2(200)not null,
-auteur varchar2(80),
-genre char(5) not null constraint fk_ouvrages_genres references genres(code),
-editeur varchar2(80));
-
-
-create table exemplaires(
-isbn number(10),
-numero number(3),
-etat char(2),
-constraint pk_exemplaires primary key(isbn, numero),
-constraint fk_exemplaires_ouvrages foreign key(isbn) references ouvrages(isbn),
-constraint ck_exemplaires_etat check (etat in('NE','BO','MO','MA', 'RE')) );
-
-
-create table membres(
-numero number(6) constraint pk_membres primary key,
-nom varchar2(80) not null,
-prenom varchar2(80) not null,
-adresse varchar2(200) not null,
-telephone char(10) not null,
-adhesion date not null,
-duree number(2) not null,
-constraint ck_membres_duree check (duree>=0));
-
-
-create table emprunts(
-numero number(10) constraint pk_emprunts primary key,
-membre number(6) constraint fk_emprunts_membres references membres(numero),
-creele date default sysdate
+CREATE TABLE Membres(
+numMembre     INT           CONSTRAINT pk_membres PRIMARY KEY,
+nom           VARCHAR2(30)  NOT NULL,
+prenom        VARCHAR2(30)  NOT NULL,
+adresse       VARCHAR2(100) NOT NULL,
+telephone     CHAR(10)      NOT NULL,
+dateAdhesion  DATE          NOT NULL,
+duree         INT           NOT NULL CONSTRAINT ck_membres_duree CHECK(duree IN(1, 3, 6, 12))
 );
 
-
-create table detailsemprunts(
-emprunt number(10) constraint fk_details_emprunts references emprunts(numero),
-numero number(3),
-isbn number(10),
-exemplaire number(3),
-rendule date,
-constraint pk_detailsemprunts primary key (emprunt, numero),
-constraint fk_detailsemprunts_exemplaires foreign key(isbn, exemplaire) references exemplaires(isbn, numero)
+CREATE TABLE Genres(
+codeGenre     CHAR(4)       CONSTRAINT pk_genres PRIMARY KEY,
+libelle       VARCHAR2(30)  CONSTRAINT un_genres_libelle UNIQUE
 );
+
+CREATE TABLE Ouvrages(
+isbn          CHAR(10)    CONSTRAINT pk_ouvrages PRIMARY KEY,
+titre         VARCHAR2(120) NOT NULL,
+auteur        VARCHAR2(50)  NULL,
+codeGenre     CHAR(4)       NOT NULL CONSTRAINT fk_ouvrages_genres REFERENCES Genres(codeGenre),
+editeur       VARCHAR2(30)  NULL
+);
+
+CREATE TABLE Exemplaires(
+isbn          CHAR(10)      CONSTRAINT fk_exemplaires_ouvrages REFERENCES Ouvrages(isbn),
+numExempl     INT,
+etat          CHAR(2)       NOT NULL
+                            CONSTRAINT ck_exemplaires_etat CHECK(etat IN('NE', 'BO', 'MO', 'MA', 'RE')),
+CONSTRAINT pk_exemplaires PRIMARY KEY(isbn, numExempl)
+);
+
+CREATE TABLE Emprunts(
+numEmprunt    INT           CONSTRAINT pk_emprunts PRIMARY KEY,
+numMembre     INT           NOT NULL
+                            CONSTRAINT fk_emprunts_membres REFERENCES membres(numMembre),
+dateDepart    DATE          NOT NULL
+);
+
+ALTER TABLE Emprunts MODIFY dateDepart DATE DEFAULT SYSDATE;
+
+CREATE TABLE DetailsEmprunts(
+numEmprunt    INT           CONSTRAINT fk_details_emprunts REFERENCES emprunts(numEmprunt),
+numLigne      INT,
+isbn          CHAR(10)      NOT NULL,
+numExempl     INT           NOT NULL,
+dateRetour    DATE          NULL,
+CONSTRAINT pk_details PRIMARY KEY(numEmprunt, numLigne),
+CONSTRAINT fk_details_exemplaires FOREIGN KEY(isbn, numExempl) REFERENCES exemplaires(isbn, numExempl)
+);
+
+-- 1.3
+CREATE SEQUENCE sq_membres;
+
+-- 1.4
+ALTER TABLE membres ADD CONSTRAINT un_membres_nompretel UNIQUE(nom, prenom, telephone);
+
+-- 1.5
+ALTER TABLE membres ADD portable CHAR(10) NULL;
+ALTER TABLE membres ADD CONSTRAINT ck_membres_portable CHECK(portable LIKE '06%' OR portable LIKE '07%');
+
+-- 1.6
+ALTER TABLE membres DROP CONSTRAINT un_membres_nompretel;
+ALTER TABLE membres SET UNUSED(telephone);
+ALTER TABLE membres ADD CONSTRAINT un_membres_nompretel UNIQUE(nom, prenom, portable);
+-- dans la nuit
+ALTER TABLE membres DROP UNUSED COLUMNS;
+
+-- 1.7
+CREATE INDEX in_ouvrages_codeGenre ON Ouvrages(codeGenre);
+CREATE INDEX in_details_isbnNumExempl ON DetailsEmprunts(isbn, numExempl);
+CREATE INDEX in_emprunts_numMembre ON Emprunts(NumMembre);
+
+-- 1.8
+ALTER TABLE DetailsEmprunts DROP CONSTRAINT fk_details_emprunts;
+ALTER TABLE DetailsEmprunts ADD CONSTRAINT fk_details_emprunts
+FOREIGN KEY(numEmprunt) REFERENCES emprunts(numEmprunt) ON DELETE CASCADE;
+
+-- 1.9
+ALTER TABLE exemplaires MODIFY etat CHAR(2) DEFAULT 'NE';
+
+-- 1.10
+CREATE SYNONYM abonnes FOR membres;
+
+-- 1.11
+RENAME DetailsEmprunts TO Details;
+
+
 
 
 -- 1.3 Création d'une séquence
@@ -79,67 +116,57 @@ alter table exemplaires modify (etat char(2) default 'NE');
 -- 1.11
 rename detailsemprunts to details;
 
+/*2.1 Insertions*/
+DELETE FROM Emprunts;
+DELETE FROM Membres;
+DELETE FROM Exemplaires;
+DELETE FROM Ouvrages;
+DELETE FROM Genres;
 
+INSERT INTO genres VALUES('REC', 'Récit');
+INSERT INTO genres VALUES('POL', 'Policier');
+INSERT INTO genres VALUES('BD', 'Bande Dessinée');
+INSERT INTO genres VALUES('INF', 'Informatique');
+INSERT INTO genres VALUES('THE', 'Théatre');
+INSERT INTO genres VALUES('ROM', 'Roman');
 
--- 2.1 Ajout d'informations dans une table
+INSERT INTO Ouvrages VALUES(2203314168,'LEFRANC-L''utimatum','Martin, Carin','BD','Casterman');
+INSERT INTO Ouvrages VALUES(2746021285,'HTML entraînez-vous pour maîtriser le code source','Luc Van Lancker','INF','ENI');
+INSERT INTO Ouvrages VALUES(2746026090,'Oracle 10g SQL, PL/SQL, SQL*Plus','J. Gabillaud','INF','ENI');
+INSERT INTO Ouvrages VALUES(2266085816,'Pantagruel','François Rabelais','ROM','POCKET');
+INSERT INTO OUVRAGES VALUES(2266091611,'Voyage au centre de la terre','Jules Verne','ROM','POCKET');
+INSERT INTO OUVRAGES VALUES(2253010219,'Le crime de l''orient express','Agatha Christie','POL','Livre de poche');
+INSERT INTO OUVRAGES VALUES(2070400816,'Le bourgeois gentilhomme','Molière','THE','Galimard');
+INSERT INTO OUVRAGES VALUES(2070367177,'Le curé de tours','Honoré de Balzac','ROM','Galimard');
+INSERT INTO OUVRAGES VALUES(2080720872,'Boule de suif','Guy de Maupassant','REC','Flamarion');
+INSERT INTO OUVRAGES VALUES(2877065073,'La gloire de mon père','Marcel Pagnol','ROM','Fallois');
+INSERT INTO OUVRAGES VALUES(2020549522,'L''aventure des manuscrits de la mer morte',null,'REC','Seuil');
+INSERT INTO OUVRAGES VALUES(2253006327,'Vingt mille lieues sous les mers','Jules Verne','ROM','LGF');
+INSERT INTO OUVRAGES VALUES(2038704015,'De la terre à la lune','Jules Verne','ROM','Larousse');
 
-insert into genres values ('REC', 'récit');
-insert into genres values ('POL', 'policier');
-insert into genres values ('BD', 'Bande Dessinée');
-insert into genres values ('INF', 'Informatique');
-insert into genres values ('THE', 'Théâtre');
-insert into genres values ('ROM', 'Roman');
-insert into ouvrages values (2203314168,'LEFRANC-L''ultimatum', 'Martin, Carin', 'BD', 'Casterman');
-insert into ouvrages values (2746021285,'HTML entraînez-vous pour maitriser le code source', 'Luc Van Lancker', 'INF', 'ENI');
-insert into ouvrages values (2266085816,'ORACLE 12cSQL, PL/SQL, SQL*plis', 'J.Gabillaud', 'INF', 'ENI');
-insert into ouvrages values (2746026090,'Pantagruel', 'François Rabelais', 'ROM', 'POCKET');
-insert into ouvrages values (2266091611,'Voyage au centre de la terre', 'Jules Vernes', 'ROM', 'POCKET');
-insert into ouvrages values (2253010219,'Le crime de l''Orient Express', 'Agatha Cristie', 'POL', 'Livre De Poche');
-insert into ouvrages values (2070400816,'Le Bourgeois Gentilhomme', 'Molière', 'THE', 'Gallimard');
-insert into ouvrages values (2070367177,'Le curé de Tours', 'Honoré de Balzac', 'ROM', 'Gallimard');
-insert into ouvrages values (2080720872,'Boule de suif','Guy de Maupassant', 'REC', 'Flammarion');
-insert into ouvrages values (2877065073,'La Gloire de mon père','Marcel Pagnol','ROM','Fallois');
-insert into ouvrages values (2020549522,'L''aventure des manuscrits de la mer morte', NULL, 'REC', 'Seuil');
-insert into ouvrages values (2253006327,'Vingt mille lieues sous les mers', 'Jules Vernes', 'ROM', 'LGF');
-insert into ouvrages values (2038704015,'De la terre à la lune', 'Jules vernes', 'ROM', 'Larousse');
-insert into exemplaires values (2203314168,1,'MO');
-insert into exemplaires values (2203314168,2,'BO');
-insert into exemplaires values (2203314168,3,'NE');
-insert into exemplaires values (2746021285,1,'BO');
-insert into exemplaires values (2746026090,1,'BO');
-insert into exemplaires values (2746026090,2,'MO');
-insert into exemplaires values (2266091611,1,'BO');
-insert into exemplaires values (2266091611,2,'MO');
-insert into exemplaires values (2253010219,1,'BO');
-insert into exemplaires values (2253010219,2,'MO');
-insert into exemplaires values (2070400816,1,'BO');
-insert into exemplaires values (2070400816,2,'MO');
-insert into exemplaires values (2070367177,1,'BO');
-insert into exemplaires values (2070367177,2,'MO');
-insert into exemplaires values (2080720872,1,'BO');
-insert into exemplaires values (2080720872,2,'MO');
-insert into exemplaires values (2877065073,1,'BO');
-insert into exemplaires values (2877065073,2,'MO');
-insert into exemplaires values (2020549522,1,'BO');
-insert into exemplaires values (2020549522,2,'MO');
-insert into exemplaires values (2253006327,1,'BO');
-insert into exemplaires values (2253006327,2,'MO');
-insert into exemplaires values (2038704015,1,'BO');
-insert into exemplaires values (2038704015,2,'MO');
+INSERT INTO exemplaires SELECT isbn, 1, 'BO' FROM ouvrages;
+INSERT INTO exemplaires SELECT isbn, 2, 'MO' FROM ouvrages;
+DELETE FROM exemplaires WHERE isbn = 2746021285 AND numExempl = 2;
+UPDATE exemplaires SET etat='MO' WHERE isbn = 2203314168 AND numExempl = 1;
+UPDATE exemplaires SET etat='NE' WHERE isbn = 2203314168 AND numExempl = 2;
+INSERT INTO exemplaires VALUES(2203314168, 3, 'NE');
 
--- 2.2 Utilisation d'une séquence
-INSERT INTO membres VALUES (sq_membre.nextvalue, 'ALBERT','Anne','13 rue des alpes', '0601020304', sysdate-60, 1);
-INSERT INTO membres VALUES (sq_membre.nextvalue, 'BERNAUD','Barnabé','6 rue des bécasses', '0601020305', sysdate-10, 3);
-INSERT INTO membres VALUES (sq_membre.nextvalue, 'CUVARD','Camille','52 rue des cerisiers', '0601020306', sysdate-100, 6);
-INSERT INTO membres VALUES (sq_membre.nextvalue, 'DUPOND','Daniel','11 rue des daoùs', '0601020307', sysdate-250, 12);
-INSERT INTO membres VALUES (sq_membre.nextvalue, 'EVROUX','Eglantine','34 rue des elfes', '0601020308', sysdate-150, 6);
-INSERT INTO membres VALUES (sq_membre.nextvalue, 'FREGEON','Fernand','11 rue des Francs', '0601020309', sysdate-400, 6);
-INSERT INTO membres VALUES (sq_membre.nextvalue, 'GORIT','Gaston','96 rue de la glacerie', '0601020310', sysdate-150, 1);
-INSERT INTO membres VALUES (sq_membre.nextvalue, 'HEVARD','Hector','12 rue haute', '0601020311', sysdate-250, 12);
-INSERT INTO membres VALUES (sq_membre.nextvalue, 'INGRAND','Irène','54 rue de iris', '0601020312', sysdate-50, 12);
-INSERT INTO membres VALUES (sq_membre.nextvalue, 'JUSTE','Julien','5 place des jacobins', '0601020313', sysdate-100, 6);
+/* 2.2 */
+DROP SEQUENCE sq_membres;
+CREATE SEQUENCE sq_membres;
 
--- 2.3 Exécution d'un script
+INSERT INTO membres VALUES(sq_membres.NEXTVAL, 'ALBERT', 'Anne', '13 rue des Alpes', SYSDATE-60, 1, '0601020304');
+INSERT INTO membres VALUES(sq_membres.NEXTVAL, 'BERNARD', 'Barnabé', '6 rue des bécasses', SYSDATE-10, 3, '0602030105');
+INSERT INTO membres VALUES(sq_membres.NEXTVAL, 'CUVARD', 'Camille', '52 rue des cerisiers', SYSDATE-100, 6, '0602010509');
+INSERT INTO membres VALUES(sq_membres.NEXTVAL, 'DUPOND', 'Daniel', '11 rue des daims', SYSDATE-250, 12, '0610236515');
+INSERT INTO membres VALUES(sq_membres.NEXTVAL, 'EVROUX', 'Eglantine', '34 rue des elfes', SYSDATE-150, 6, '0658963125');
+INSERT INTO membres VALUES(sq_membres.NEXTVAL, 'FREGEON', 'Fernand', '11 rue des Francs', SYSDATE-400, 6, '0602036987');
+INSERT INTO membres VALUES(sq_membres.NEXTVAL, 'GORIT', 'Gaston', '96 rue de la glacerie', SYSDATE-150, 1, '0684235781');
+INSERT INTO membres VALUES(sq_membres.NEXTVAL, 'HEVRARD', 'Hector', '12 rue haute', SYSDATE-250, 12, '0608546578');
+INSERT INTO membres VALUES(sq_membres.NEXTVAL, 'INGRAND', 'Irène', '54 rue de iris', SYSDATE-50, 12, '0605020409');
+INSERT INTO membres VALUES(sq_membres.NEXTVAL, 'JUSTE', 'Julien', '5 place des Jaccobins', SYSDATE-100, 6, '0603069876');
+
+/* 2.3 */
 insert into emprunts(numero, membre, creele) values(1,1,sysdate-200);
 insert into emprunts(numero, membre, creele) values(2,3,sysdate-190);
 insert into emprunts(numero, membre, creele) values(3,4,sysdate-180);
@@ -195,25 +222,102 @@ insert into details(emprunt, numero, isbn, exemplaire, rendule) values(20,2,2253
 Insert into ouvrages (isbn, titre, auteur, genre, editeur) values (2080703234, 'Cinq semaines en ballon', 'Jules Verne', 'ROM', 'Flammarion');
 
 
--- 2.4 Extraction simple d'informations
 
-SELECT * FROM genres;
-SELECT * FROM ouvrages;
+/* 2.4 */
+SELECT * FROM membres;
 SELECT * FROM exemplaires;
+SELECT * FROM ouvrages;
 SELECT * FROM emprunts;
 SELECT * FROM details;
-SELECT * FROM membres;
+SELECT * FROM genres;
 
--- 2.5 Activation de l'historique des mouvements
-ALTER TABLE membres ENABLE ROW MOVEMENT;
-ALTER TABLE details ENABLE ROW MOVEMENT;
+/* 2.5 */
+ALTER TABLE Membres ENABLE ROW MOVEMENT;
+ALTER TABLE Details ENABLE ROW MOVEMENT;
 
--- 2.6 Ajout d'une colonne
+/* 2.6 */
+ALTER TABLE Emprunts ADD etat CHAR(2) DEFAULT 'EC'
+  CONSTRAINT ck_emprunts_etat CHECK(etat IN('EC','RE'));
+-- maj des données à 'RE'
+UPDATE Emprunts SET etat='RE';
+-- maj conditionnelle à 'EC'
+UPDATE Emprunts SET etat='EC'
+WHERE numEmprunt IN (SELECT numEmprunt FROM Details WHERE dateRetour IS NULL);
 
-ALTER TABLE emprunts ADD (etat char(2) DEFAULT 'EC');
-ALTER TABLE emprunts ADD CONSTRAINT ck_etat_rendu CHECK (etat IN ('EC', 'RE'));
+/* 2.8 Suppresion de lignes */
 
-UPDATE emprunts
-SET etat = 'RE'
-WHERE etat = 'EC' AND  numero not in (select emprunt from details where rendule is null);
- 
+SELECT * FROM EXEMPLAIRES WHERE etat='MA';
+
+DELETE FROM EXEMPLAIRES WHERE etat='MA';
+
+-- Ajout d'un enregistrement dans la table Exemplaires dont l'etat est 'MA'
+
+INSERT INTO EXEMPLAIRES VALUES ('2203314168', '4', 'MA');
+
+/* 2.9 */
+SELECT isbn, titre, COALESCE(auteur, '-') auteur, codeGenre, editeur
+FROM ouvrages;
+
+/* 2.10 */
+SELECT numMembre, nom, prenom, adresse, dateAdhesion, duree, titre
+FROM membres
+NATURAL JOIN emprunts
+NATURAL JOIN details
+NATURAL JOIN ouvrages
+WHERE dateDepart < SYSDATE - 14 AND dateRetour IS NULL;
+
+/* 2.11 */
+SELECT codeGenre, COUNT(*) nombre
+FROM Ouvrages
+GROUP BY codeGenre;
+
+/* 2.12 */
+SELECT AVG(dateRetour - dateDepart) dureeMoyenne
+FROM emprunts
+NATURAL JOIN details
+WHERE dateRetour IS NOT NULL;
+
+/* 2.13 */
+SELECT codeGenre, AVG(dateRetour - dateDepart) dureeMoyenne
+FROM emprunts
+NATURAL JOIN details
+NATURAL JOIN ouvrages
+WHERE dateRetour IS NOT NULL
+GROUP BY codeGenre;
+
+/* 2.14 */
+SELECT titre, COUNT(*) nbEmprunts
+FROM details NATURAL JOIN ouvrages
+WHERE renduLe IS NULL OR renduLe > ADD_MONTHS(SYSDATE, -12)
+GROUP BY isbn, titre
+HAVING COUNT(*) > 10;
+
+/* 2.15 */
+SELECT o.*, COALESCE(numero, 0) exemplaire
+FROM Ouvrages o LEFT OUTER JOIN exemplaires e ON e.isbn = o.isbn
+ORDER BY o.isbn, e.numero;
+
+/* 2.16 */
+CREATE OR REPLACE VIEW vueNbEmpruntsParMembre AS (
+  SELECT m.numero, nom, prenom, COUNT(*) nbEmpruntsEnCours
+  FROM membres m
+  INNER JOIN emprunts e ON m.numero = e.membre
+  INNER JOIN details d ON e.numero = d.emprunt
+  WHERE renduLe IS NULL
+  GROUP BY nom, prenom, m.numero
+);
+-- test de la vue
+SELECT * FROM vueNbEmpruntsParMembre;
+
+/* 2.17 */
+CREATE OR REPLACE VIEW vueNbEmpruntsParOuvrages AS (
+  SELECT isbn, COUNT(*) nbEmprunts
+  FROM details NATURAL JOIN ouvrages
+  GROUP BY isbn
+);
+-- verif de la creation de la vue
+SELECT * FROM vueNbEmpruntsParOuvrages;
+
+/* 2.18 */
+SELECT * FROM Membres
+ORDER BY nom, prenom;
